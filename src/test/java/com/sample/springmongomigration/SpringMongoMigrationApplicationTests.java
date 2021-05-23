2489bdb0f770327997abbbff049b880fc4814920
aek <konan.kouame@soprasteria.com>
2021-05-23 20:33:49 +0200

    init architecture

---
+++ src/test/java/com/sample/springmongomigration/SpringMongoMigrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sample.springmongomigration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMongoMigrationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
